7086d54c8b9c2f03f908a72c213a5ca6699de45c
rikibuckingham <contact@rikibuckingham.earth>
2026-09-18 20:41:31 +0100

    Use Communa8 Tasks test and auth identifiers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,14 +46,14 @@ android {
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
-        testApplicationId = "org.tasks.test"
+        testApplicationId = "org.communa8.tasks.test"
         applicationId = libs.versions.applicationId.get()
         versionCode = libs.versions.versionCode.get().toInt()
         versionName = libs.versions.versionName.get()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         minSdk = libs.versions.android.minSdk.get().toInt()
         testInstrumentationRunner = "org.tasks.TestRunner"
-        manifestPlaceholders["appAuthRedirectScheme"] = "org.tasks"
+        manifestPlaceholders["appAuthRedirectScheme"] = "org.communa8.tasks"
     }
 
     signingConfigs {
